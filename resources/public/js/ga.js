288df8ff45e670e9afc49cfd0b516fe5e1bef4f2
Sathish <sathish316@gmail.com>
2011-10-06 19:39:06 +0200

    Loading google analytics in jquery onload

--- resources/public/js/ga.js
+++ resources/public/js/ga.js
@@ -1,4 +1,6 @@
-  var _gaq = _gaq || [];
+var _gaq;
+$(function(){
+  _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-26170908-1']);
   _gaq.push(['_trackPageview']);
 
@@ -7,3 +9,4 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
+});
