8cb55e035e0c3784cc48353cbf3ed03deaf7b507
Sathish <sathish316@gmail.com>
2011-10-06 04:03:15 +0200

    Marker for failure

--- resources/public/js/koan.js
+++ resources/public/js/koan.js
@@ -5,6 +5,19 @@ $(function() {
   var ClojureMode = require("ace/mode/clojure").Mode;
   editor.getSession().setMode(new ClojureMode());
 
+  function findErrorLine(assertionError){
+    var line = assertionError.split("\n")[1];
+    console.log(line);
+    var lineNumber = -1
+    $(editor.getSession().getValue().split("\n")).each(function(index, currentLine){
+      if(line == currentLine.trim()){
+        lineNumber = index;
+        return false;
+      }
+     });
+    return lineNumber;
+  }
+
   $('.run-koan').click(function(){
     var code = editor.getSession().getValue();
     $.ajax({
@@ -15,6 +28,10 @@ $(function() {
         console.log(data);
         $('#status').html(data.status);
         $('#status').removeClass().addClass(data.status);
+        if (data.status == 'FAIL'){
+          var lineNumber = findErrorLine(data.message);
+          editor.getSession().setAnnotations([{ row: lineNumber, column: 0, text: "Assertion error", type: "error"}]);
+        }
       },
       error: function(data){
         console.log("failed");
